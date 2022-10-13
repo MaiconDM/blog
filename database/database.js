00c780b3f1b714636c9a6641d776a0929d98f8a0
MaiconDM <60263917+MaiconDM@users.noreply.github.com>
2022-10-13 17:27:16 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const connection = new Sequelize('guiapress', 'root', '352699dm',{
+const connection = new Sequelize('guiapress', 'root', 'SenhaBD',{
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
